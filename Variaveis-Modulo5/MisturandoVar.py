573795f9dcbbeba14c4b3183968a3ec8bfa89a09
unknown <isabella14brandao@gmail.com>
2024-04-03 18:12:25 -0300

    Misturando variáveis em Py

---
+++ Variaveis-Modulo5/MisturandoVar.py
@@ -0,0 +1,10 @@
+#Misturando tipos de variáveis em Py 
+
+#faturamento = 1000
+#print("O faturamento da loja foi" + faturamento)
+"""O print vai gerar um erro - só podemos concatenar string com String """
+
+#Para corrigir o erro precisamos tranformar o número em uma String - formato de texto para conseguir concaenar
+
+faturamentos = 1000
+print("O faturamento da loja foi " + str(faturamentos))
